0d6fd4a35ddc91fa225a3da70fbbe7c29f0f4ef4
BernhardGundel <bernhard.gundel@htwg-konstanz.de>
2019-04-12 11:35:11 +0200

    Die Class added

--- src/main/scala/de/htwg/se/shutthebox/model/ShutTheBox.scala
+++ src/main/scala/de/htwg/se/shutthebox/model/ShutTheBox.scala
@@ -1,5 +1,7 @@
 package de.htwg.se.shutthebox.model
 
+import scala.collection.mutable.ListBuffer
+
 object ShutTheBox {
   def main(args: Array[String]): Unit = {
     val name = "ShutTheBox"
@@ -8,17 +10,26 @@ object ShutTheBox {
   }
 }
 
-
+// Field, which implements 9 cells in List
 class Field {
-  val field = (Array.ofDim[Cell](9))
-  for (i <- 0 to 8) {
-    field(i) = new Cell
-    field(i).value = i + 1
+  val field = Array.ofDim[Cell](9)
+  for (i <- 1 to 9) {
+    field(i) = new Cell(i)
   }
 }
 
-class Cell {
-  var value = 0
+class Cell(val x : Integer) {
+  var value = x
   var isShut = false;
 }
 
+class Die() {
+  val rnd = scala.util.Random
+  val start = 1
+  val end   = 6
+  var value = roll
+
+  def roll: Integer = {
+    start + rnd.nextInt((end - start) + 1)
+  }
+}
