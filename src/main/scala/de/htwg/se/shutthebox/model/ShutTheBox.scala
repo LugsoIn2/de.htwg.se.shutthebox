042a3ad6468096e344b1d29f1182306938d99a7e
BernhardGundel <bernhard.gundel@htwg-konstanz.de>
2019-04-12 09:34:43 +0200

    Test commit

--- src/main/scala/de/htwg/se/shutthebox/model/ShutTheBox.scala
+++ src/main/scala/de/htwg/se/shutthebox/model/ShutTheBox.scala
@@ -2,26 +2,23 @@ package de.htwg.se.shutthebox.model
 
 object ShutTheBox {
   def main(args: Array[String]): Unit = {
-    val program = "ShutTheBox"
-    println(program)
-  }
+    val name = "ShutTheBox"
+    val spielfeld = new Field
 
-  object Field {
-    val field1 = (Array.ofDim[Cell](9))
-    for (i <- 0 to 8) {
-      field1(i) = new Cell
-      field1(i).value = i + 1
-
-    }
-  }
-
-  class Cell() {
-    var value = 0
-    val isShut = false;
   }
 }
 
-object Testobject {
-  val testtest = "Hallo du"
-  println(testtest)
+
+class Field {
+  val field = (Array.ofDim[Cell](9))
+  for (i <- 0 to 8) {
+    field(i) = new Cell
+    field(i).value = i + 1
+  }
 }
+
+class Cell {
+  var value = 0
+  var isShut = false;
+}
+
--- src/test/scala/de/htwg/se/shutthebox/model/ShutTheBoxTest.scala
+++ src/test/scala/de/htwg/se/shutthebox/model/ShutTheBoxTest.scala
@@ -3,15 +3,31 @@ package de.htwg.se.shutthebox.model
 import org.scalatest._
 
 class ShutTheBoxTest extends WordSpec with Matchers {
-    "A Cell" should {
-      "a value" in {
-        Cell.value should be >= 0
-      }
-    }
-  "A Cell" should {
+  val field = new Field()
+  val cell = new Cell()
+
+  "A ShutTheBox" should {
     "a value" in {
-      Cell.value should be <= 9
+      
     }
   }
 
+  "A Cell" should {
+    "a value" in {
+      cell shouldBe a [Cell]
+      cell.value should be >= 0
+      cell.value should be <= 9
+      cell.isShut should be(false)
+    }
+  }
+
+  "A Field" should {
+    "a value" in {
+      field shouldBe a [Field]
+      field.field shouldBe a [Array[Cell]]
+    }
+  }
 }
+
+
+
