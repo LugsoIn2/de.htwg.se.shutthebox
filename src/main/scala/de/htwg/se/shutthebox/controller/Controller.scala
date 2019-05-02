86497b3240bed47b5520b10b9e7f19d0365fc181
LugsoIn2 <timmy.pfau@lugso.de>
2019-05-02 12:07:56 +0200

    MVC-Structure

--- src/main/scala/de/htwg/se/shutthebox/controller/Controller.scala
+++ src/main/scala/de/htwg/se/shutthebox/controller/Controller.scala
@@ -1,5 +1,8 @@
 package de.htwg.se.shutthebox.controller
 
+import de.htwg.se.shutthebox.model._
+
 class Controller {
+
 
 }
---
+++ src/main/scala/de/htwg/se/shutthebox/util/Observable.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.shutthebox.util
+
+trait Observer {
+  def update: Unit
+}
+
+class Observable {
+
+}
